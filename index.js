d7115ccea291915151197531558c479d1bbe057f
FelipeZamara <felipezamara9@gmail.com>
2024-09-17 22:54:34 -0300

    C e R do CRUD

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express =  require('express')
+const Person = require('./models/Person')
 const app =  express()
+const mongoose = require("mongoose")
 
-app.listen(3000)
 app.use(
     express.urlencoded({
         extended: true
@@ -13,4 +14,36 @@ app.use(express.json())
 //rotas
 app.get('/', (req, res)=>{
     res.json({message:"Rodou!"})
+})
+
+//Create
+app.post("/person", async (req, res) =>{
+    const { name, salary, approved} = req.body;
+
+    const person = {
+        name,
+        salary,
+        approved,
+    }
+
+    try{
+        await Person.create(person)
+        res.status(201).json({message: "Pessoa inserida no sistema com sucesso!"})
+    } catch (error){
+        res.status(500).json({erro: error})
+    }
+})
+//read
+app.get("/person", async (req, res)=>{
+    try{
+        const peaple = await Person.find()
+        res.status(200).json(peaple)
+    }catch (error){
+        res.status(500).json({erro: error})
+    }
+})
+
+mongoose.connect("mongodb://localhost:27017").then(()=>{
+    console.log("Uhul, conectamos!")
+    app.listen(3000)
 })
